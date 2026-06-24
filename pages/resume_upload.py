5632f2297e0d46d6ce339ca5e6232c2e2e06675b
Palak Narang <palaknarang@Palaks-MacBook-Air.local>
2026-06-24 12:55:19 +0530

    fix resume session state

--- pages/resume_upload.py
+++ pages/resume_upload.py
@@ -8,5 +8,11 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
+
+    # Temporary store filename as resume text
+    st.session_state["resume_text"] = uploaded_file.name
+
     st.success("Resume Uploaded Successfully!")
+
     st.write("Filename:", uploaded_file.name)
+    st.write("Size:", round(uploaded_file.size/1024, 2), "KB")
