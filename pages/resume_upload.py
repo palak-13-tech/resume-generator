f0859eff5b5479dd561086eddd0151d920104b19
Palak Narang <palaknarang@Palaks-MacBook-Air.local>
2026-06-24 15:02:38 +0530

    remove pdfplumber

--- pages/resume_upload.py
+++ pages/resume_upload.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PyPDF2 import PdfReader
 
 st.title("Resume Upload")
 
@@ -9,14 +8,5 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
-    reader = PdfReader(uploaded_file)
-
-    text = ""
-    for page in reader.pages:
-        page_text = page.extract_text()
-        if page_text:
-            text += page_text
-
-    st.session_state["resume_text"] = text
-
+    st.session_state["resume_text"] = uploaded_file.name
     st.success("Resume Uploaded Successfully!")
