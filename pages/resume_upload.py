d0abfc3462b09990ac0e7b232af16ac189a004df
Palak Narang <palaknarang@Palaks-MacBook-Air.local>
2026-06-24 14:53:09 +0530

    remove pdfplumber

--- pages/resume_upload.py
+++ pages/resume_upload.py
@@ -1,22 +1,9 @@
 import streamlit as st
-import pdfplumber
 
 st.title("Resume Upload")
 
-uploaded_file = st.file_uploader(
-    "Upload Resume",
-    type=["pdf"]
-)
+uploaded_file = st.file_uploader("Upload Resume", type=["pdf"])
 
 if uploaded_file:
-    text = ""
-
-    with pdfplumber.open(uploaded_file) as pdf:
-        for page in pdf.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text += page_text + "\n"
-
-    st.session_state["resume_text"] = text
-
+    st.session_state["resume_text"] = uploaded_file.name
     st.success("Resume Uploaded Successfully!")
